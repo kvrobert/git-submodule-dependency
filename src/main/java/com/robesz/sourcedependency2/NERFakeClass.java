bb819dd7cbe696703bb4a76769999ed0bd4133d5
Robesz <you@example.com>
2019-08-05 13:51:49 +0200

    submodule added as dependency

---
+++ src/main/java/com/robesz/sourcedependency2/NERFakeClass.java
@@ -0,0 +1,8 @@
+package com.robesz.sourcedependency2;
+
+import com.precognox.tas.tagger.ner.dto.NERRequest;
+
+public class NERFakeClass {
+
+    NERRequest nerRequest;
+}
--- src/main/java/com/robesz/sourcedependency2/SourceDependency2Application.java
+++ src/main/java/com/robesz/sourcedependency2/SourceDependency2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SourceDependency2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SourceDependency2Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SourceDependency2Application.class, args);
+	}
 
 }
