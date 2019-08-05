b081df88b3ba410a0107e6ec25bcac7745737684
Robesz <you@example.com>
2019-08-05 13:37:45 +0200

    sub module added

---
+++ src/main/java/com/robesz/sourcedependency2/SourceDependency2Application.java
@@ -0,0 +1,13 @@
+package com.robesz.sourcedependency2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceDependency2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceDependency2Application.class, args);
+    }
+
+}
